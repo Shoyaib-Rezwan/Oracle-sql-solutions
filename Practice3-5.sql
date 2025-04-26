2cd4cc50b7264f05993104b4b9359f9d087ce4a9
Shoyaib <shoyaib.rezwan@gmail.com>
2025-04-26 23:41:22 +0600

    26/4/25

---
+++ Practice3-5.sql
@@ -0,0 +1,28 @@
+--(a)
+--(i)
+-- SELECT TO_CHAR(HIRE_DATE, 'Ddth Month, YYYY') FROM EMPLOYEES;
+
+--(ii)
+SELECT TO_CHAR(HIRE_DATE,'DD Month, YYYY') FROM EMPLOYEES;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
